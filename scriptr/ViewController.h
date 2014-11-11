005a7a95d1291a01dbea83f5dd77d8972f6544db
Andrew Brandt <abrandt@makeschool.com>
2014-11-11 11:25:18 -0800

    first commit

---
+++ scriptr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scriptr
+//
+//  Created by Andrew Brandt on 11/11/14.
+//  Copyright (c) 2014 makeschool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ scriptr/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  scriptr
+//
+//  Created by Andrew Brandt on 11/11/14.
+//  Copyright (c) 2014 makeschool. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
